dc52575055f543d6511abbecfceaf3e4d03a372b
sebasquirarte <sebastianquirajus@outlook.com>
2025-07-02 12:19:32 -0600

    Add files via upload

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+#' Hello World Function
+#'
+#' A simple function that prints hello world
+#'
+#' @return A character string
+#' @export
+#' @examples
+#' hello()
+hello <- function() {
+  return("Hello, World!")
+}
