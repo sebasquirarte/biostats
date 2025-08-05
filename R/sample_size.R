6d859b46cb2b3f79bd815b7a6fb2fcf182141542
angelacmon <amontanx@gmail.com>
2025-08-05 16:20:02 -0600

    Update sample_size

--- R/sample_size.R
+++ R/sample_size.R
@@ -20,6 +20,7 @@
 #' @param delta Numeric. Margin of clinical interest. Required for non-equality tests.
 #'   Must be negative for non-inferiority and positive for superiority/equivalence.
 #'   Default is NULL.
+#' @param dropout_rate Numeric. Study discontinuation rate expected in the study. Default is 0.
 #' @param k Numeric. Allocation ratio (n1/n2) for two-sample tests. Default is 1.
 #'
 #' @return Invisibly returns a list containing:
@@ -36,6 +37,7 @@
 #'   \item{x2}{Numeric. Value of the mean or proportion for group 2 (control group or reference value).}
 #'   \item{SD}{Numeric. Standard deviation.}
 #'   \item{delta}{Numeric. Margin of clinical interest.}
+#'   \item{dropout_rate}{Numeric. Study discontinuation rate expected in the study.}
 #'   \item{k}{Numeric. Allocation ratio (n1/n2) for two-sample tests.}
 #'   \item{z_alpha}{Numeric. Z-score corresponding to the significance level alpha.}
 #'   \item{z_beta}{Numeric. Z-score corresponding to the Type II error rate beta.}
@@ -56,16 +58,17 @@
 #'             delta = 0.05,
 #'             alpha = 0.05,
 #'             beta = 0.20)
-#' # Two-sample parallel non-inferiority test for means
+#' # Two-sample parallel non-inferiority test for proportions w/ 10% dropout rate
 #' sample_size(sample = 'two-sample',
-#'             design = 'parallel',
-#'             outcome = 'mean',
+#'             design = "parallel",
+#'             outcome = 'proportion',
 #'             type = 'non-inferiority',
-#'             x1 = 5.0,
-#'             x2 = 5.0,
-#'             SD = 0.1,
-#'             delta = -0.05,
-#'             k = 1)
+#'             x1 = 0.85,
+#'             x2 = 0.65,
+#'             alpha = 0.05,
+#'             beta = 0.20,
+#'             delta = -0.1,
+#'             dropout_rate = 0.1)
 #' # Two-sample crossover non-inferiority test for means
 #' sample_size(sample = 'two-sample',
 #'             design = "crossover",
@@ -88,25 +91,33 @@ sample_size <- function(sample = c('one-sample', 'two-sample'),
                         x2 = NULL,
                         SD = NULL,
                         delta = NULL,
+                        dropout_rate = 0,
                         k = 1) {
-
   # Input validation
   sample <- match.arg(sample)
   outcome <- match.arg(outcome)
   type <- match.arg(type)
+
   if (is.null(x1) || is.null(x2)) stop("Both x1 and x2 must be specified.")
   if (length(x1) > 1) stop("x1 must be a single value.")
   if (sample == "two-sample") {
     if (is.null(design)) stop("design must be specified for two-sample tests.")
     design <- match.arg(design, c('parallel', 'crossover'))
   }
-  needs_sd <- !(sample == 'two-sample' && design == 'parallel' && outcome == 'proportion')
-  if (needs_sd && is.null(SD)) stop("SD must be specified for this test configuration.")
+  if (!(sample == 'two-sample' && design == 'parallel' && outcome == 'proportion') && is.null(SD)) stop("SD must be specified for this test configuration.")
+  if ((sample == 'two-sample' && design == 'parallel' && outcome == 'proportion') && !is.null(SD)) warning("SD is not needed for this test configuration.")
   if (type != "equality") {
     if (is.null(delta)) stop(sprintf("delta must be provided for %s tests", type))
     if (type == 'non-inferiority' && delta >= 0) stop("delta must be negative for non-inferiority.")
     if (type %in% c('superiority', 'equivalence') && delta <= 0) stop("delta must be positive.")
+  } else {
+    if (!is.null(delta)) {
+      warning("delta is not needed for this test configuration.")
+    }
   }
+  if (!is.numeric(beta) || beta <= 0 || beta >= 1) stop("beta must be a positive decimal bigger than 0 and less than 1.")
+  if (!is.numeric(alpha) || alpha <= 0 || alpha >= 1) stop("alpha must be a positive decimal bigger than 0 and less than 1.")
+  if (!is.numeric(dropout_rate) || dropout_rate < 0 || dropout_rate >= 1) stop("dropout_rate must be a positive decimal between 0 and 1.")
 
   # Calculate z-scores
   z_alpha <- if (type == 'equality') qnorm(1 - alpha/2) else qnorm(1 - alpha)
@@ -139,31 +150,47 @@ sample_size <- function(sample = c('one-sample', 'two-sample'),
   n1 <- if (sample == 'two-sample' && design == 'parallel') ceiling(k * n2) else n2
   total <- if (sample == 'one-sample') n2 else if (design == 'parallel') n2 + n1 else 2 * n2
 
+  # Check for infinite and stop early
+  if (!is.finite(n1) || !is.finite(n2) || !is.finite(total)) stop("Sample size calculation resulted in infinite or undefined value. Check delta and group difference.")
+
   # Print results
   cat("\nSample Size Calculation Summary\n\n")
   cat("Test type:", type, "\n")
-  cat("Design:", if (sample == 'two-sample') paste(design, ",", sample) else sample, "\n")
+  cat("Design:", if (sample == 'two-sample') sprintf("%s, %s\n", design, sample) else sprintf("%s\n", sample))
   cat("Outcome:", outcome, "\n")
   cat(sprintf("Alpha (\u03b1): %.2f\n", alpha))
   cat(sprintf("Beta (\u03b2): %.2f\n", beta))
   cat(sprintf("Power: %.0f%%\n\n", (1 - beta) * 100))
 
   cat("Parameters:\n")
-  cat(sprintf("x1: %.2f\n", x1))
-  cat(sprintf("x2: %.2f\n", x2))
+  cat(sprintf("x1 (treatment): %.2f\n", x1))
+  cat(sprintf("x2 (control/reference): %.2f\n", x2))
   cat(sprintf("Difference (x1 - x2): %.2f\n", diff))
+  if (dropout_rate != 0) cat(sprintf("Dropout rate: %.0f%%\n", dropout_rate * 100))
   if (!is.null(SD)) cat(sprintf("Standard Deviation (\u03c3): %.2f\n", SD))
   if (sample == 'two-sample') cat(sprintf("Allocation Ratio (k): %.2f\n", k))
   if (!is.null(delta)) cat(sprintf("Delta (\u03b4): %.2f\n", delta))
 
-  cat("\nRequired Sample Size:\n")
-  if (sample == 'one-sample') {
-    cat(sprintf("n = %d\n", total))
+  if (dropout_rate == 0) {
+    cat("\nRequired Sample Size\n")
+    if (sample == 'one-sample') {
+      cat(sprintf("n = %d\n", total))
+    } else {
+      cat(sprintf("n1 = %d\n", n1))
+      cat(sprintf("n2 = %d\n", n2))
+    }
+    cat(sprintf("Total = %d\n", total))
   } else {
-    cat(sprintf("n1 = %d\n", n1))
-    cat(sprintf("n2 = %d\n", n2))
+    cat("\nRequired Sample Size*\n")
+    if (sample == 'one-sample') {
+      cat(sprintf("n = %d\n", ceiling(n2 + (dropout_rate * n2))))
+    } else {
+      cat(sprintf("n1 = %d\n", ceiling(n1 + (dropout_rate * n1))))
+      cat(sprintf("n2 = %d\n", ceiling(n2 + (dropout_rate * n2))))
+    }
+    cat(sprintf("Total = %d\n\n", (ceiling(n1 * (1 + dropout_rate)) + (ceiling(n2 * (1 + dropout_rate))))))
+    cat(sprintf("*Sample size inflated by %.0f%% to account for potential dropout.\n\n", dropout_rate * 100))
   }
-  cat(sprintf("Total = %d\n\n", total))
 
   invisible(list(
     n1 = n1,
